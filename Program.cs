9a0f10704197a23535d5f1295fd6f270b533276f
coolpagani <coolpagani@gmail.com>
2022-09-07 19:21:39 +0300

    work_47

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void TwoDimensionalArray(int a, int b)
+{
+    double[,] array = new double[a, b];
+    for (int i = 0; i < a; i++)
+        for (int q = 0; q < b; q++) array[i, q] = new Random().Next(-100, 101) + Math.Round(new Random().NextDouble(), 1);
+    for (int i = 0; i < a; i++)
+    {
+        for (int q = 0; q < b; q++) Console.Write($"{array[i, q]} ");
+    Console.WriteLine();     
+    }  
+}
+int m = 3;
+int n = 4;
+TwoDimensionalArray(m, n);
