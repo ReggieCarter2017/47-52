c2f79aa8b5e823728728fbfe484c890bba77d53b
coolpagani <coolpagani@gmail.com>
2022-09-07 19:37:55 +0300

    work_50

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿
-void TwoDimensionalArray(int a, int b)
+void TwoDimensionalArray(int a, int b, int pos)
 {
     double[,] array = new double[a, b];
     for (int i = 0; i < a; i++)
@@ -8,8 +8,14 @@ void TwoDimensionalArray(int a, int b)
     {
         for (int q = 0; q < b; q++) Console.Write($"{array[i, q]} ");
     Console.WriteLine();     
-    }  
+    }
+    Console.WriteLine();
+    if (pos / 10 < a && pos % 10 < b && pos > -1) Console.Write(array[pos / 10, pos % 10]); // Решение 50-ой задачи
+    else
+    Console.Write("Такого индекса не существует в данном двумерном массиве.");
 }
-int m = 3;
-int n = 4;
-TwoDimensionalArray(m, n);
+int m = 5;
+int n = 5;
+Console.WriteLine("Введите индекс элемента: ");
+int.TryParse(Console.ReadLine(), out int a);
+TwoDimensionalArray(m, n, a);
